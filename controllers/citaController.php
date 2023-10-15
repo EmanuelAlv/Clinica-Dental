ef324443a3790ef862ddb39c58dc51beb9c20a7a
Emanuel Alvarez <alvarezema9@gmail.com>
2023-10-15 10:56:34 -0600

    Se agrego la funcion isAuth en el archivo includes/funciones.php para evitar que se pueda acceder a las vistas de cita sin estar autenticado antes

--- controllers/citaController.php
+++ controllers/citaController.php
@@ -6,7 +6,7 @@ use MVC\Router;
 class citaController {
     public static function index(Router $router){
         session_start();
-
+        isAuth();
         $router->render('cita/index', [
             'nombre' => $_SESSION['nombre'],
             'Id' => $_SESSION['id']
