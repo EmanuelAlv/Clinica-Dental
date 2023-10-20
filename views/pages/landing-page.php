b3d1e2d6f78bfd0f304c45167e35cc2adfeedd7c
GabrielMunguia1 <141886718+GabrielMunguia1@users.noreply.github.com>
2023-10-19 19:37:17 -0600

    Add files via upload

--- views/pages/landing-page.php
+++ views/pages/landing-page.php
@@ -1,6 +1,6 @@
 <header>
         <div class="logo-container">
-        <img src="https://i.ibb.co/199X4mB/confident-Logo.png" alt="Logo">
+        <img src="https://i.ibb.co/MkqZs9f/confident-Logo.png" alt="Logo">
         <h1>ConfiDent</h1>
         </div>
         <div class="lista-container">
@@ -13,9 +13,9 @@
         </nav>
         </div>
     </header>
-
+    
     <section class="Informacion-Ubicacion"> 
-        <div class="info-leftside flex">
+        <div class="info-leftside-flex">
             <img src="https://comosa.com.gt/wp-content/uploads/2020/08/gal2.jpg" alt="Fotografia del edificio Nova Reformador donde esta la clinica">
         </div>
         <div class="info-rightside flex">
@@ -28,46 +28,43 @@
     </section>
 
         <section class="servicios">
-            <div class="contenedor-principal">
+            <div class="contenedor-principal-landing ">
             <h2>Nuestros Servicios</h2>
             <div class="contenedor-cards">
-                <div class="columna1">
                 <div class="card-servicios">
-                    <img src="https://i.ibb.co/mBqbf39/consulta.png" alt="Icono del servicio">
+                    <img src="https://i.ibb.co/hHnd3v5/consulta.png" alt="Icono del servicio">
                     <p>Consulta General</p>
                 </div>
                 
                 <div class="card-servicios">
-                    <img src="https://i.ibb.co/mBqbf39/consulta.png" alt="Icono del servicio">
+                    <img src="https://i.ibb.co/hHnd3v5/consulta.png" alt="Icono del servicio">
                     <p>Limpieza Dental</p>
                 </div>
                 <div class="card-servicios">
-                    <img src="https://i.ibb.co/mBqbf39/consulta.png" alt="Icono del servicio">
+                    <img src="https://i.ibb.co/hHnd3v5/consulta.png" alt="Icono del servicio">
                     <p>Rellenos</p>
                 </div>
                 <div class="card-servicios">
-                    <img src="https://i.ibb.co/mBqbf39/consulta.png" alt="Icono del servicio">
+                    <img src="https://i.ibb.co/hHnd3v5/consulta.png" alt="Icono del servicio">
                     <p>Blanqueamientos</p>
                 </div>
-                </div> 
-                <div class="columna2">
+
                 <div class="card-servicios">
-                    <img src="https://i.ibb.co/mBqbf39/consulta.png" alt="Icono del servicio">
+                    <img src="https://i.ibb.co/hHnd3v5/consulta.png" alt="Icono del servicio">
                     <p>Extracciones</p>
                 </div>
                 <div class="card-servicios">
-                    <img src="https://i.ibb.co/mBqbf39/consulta.png" alt="Icono del servicio">
+                    <img src="https://i.ibb.co/hHnd3v5/consulta.png" alt="Icono del servicio">
                     <p>Coronas</p>
                 </div>
                 <div class="card-servicios">
-                    <img src="https://i.ibb.co/mBqbf39/consulta.png" alt="Icono del servicio">
+                    <img src="https://i.ibb.co/hHnd3v5/consulta.png" alt="Icono del servicio">
                     <p>Puentes Fijos</p>
                 </div>
                 <div class="card-servicios">
-                    <img src="https://i.ibb.co/mBqbf39/consulta.png" alt="Icono del servicio">
+                    <img src="https://i.ibb.co/hHnd3v5/consulta.png" alt="Icono del servicio">
                     <p>Tratamiento Periodontal</p>
                 </div>
-            </div>
             </div>
             </div>
         </section>
@@ -77,21 +74,20 @@
         <p>Operador: Gabriel Alvarez <br>
            Numero: +502 36175747 </p>
     </section>
-
     <section class="contacto">
         <h2>Siguenos</h2>
         <div class="contacto-contenedor-cards flex">
             <div class="contacto-card">
-                <img src="https://i.ibb.co/4gxRWQw/circle-phone.png" alt="Llamada">
+                <img src="https://i.ibb.co/31qH7M0/circle-phone.png" alt="Llamada">
             </div>
             <div class="contacto-card">
-                <img src="https://i.ibb.co/F4fV485/whatsapp.png" alt="Whatsapp">
+                <img src="https://i.ibb.co/kGVW5d6/whatsapp.png" alt="Whatsapp">
             </div>
             <div class="contacto-card">
-                <img src="https://i.ibb.co/jhNqC3c/instagram.png" alt="Instagram">
+                <img src="https://i.ibb.co/z6Vq42Z/instagram.png" alt="Instagram">
             </div>
             <div class="contacto-card">
-                <img src="https://i.ibb.co/kKDK4n6/facebook.png" alt="Facebook">
+                <img src="https://i.ibb.co/QNQ0DNx/facebook.png" alt="Facebook">
             </div>
         </div>
     </section>
